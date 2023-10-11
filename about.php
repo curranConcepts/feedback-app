678c2bf3ed0cce1225a6e22fbbf89b00c53d47b0
Austin_Curran <a.curran1192@gmail.com>
2023-10-11 12:07:02 -0600

    initial commit

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include 'partials/header.php'; ?>
+
+    <h2>About</h2>
+
+    <p class="text-center">This is a feedback application demonstrating my understanding and knowledge of PHP and MYSQL.</p>
+
+<?php include 'partials/footer.php'; ?>
+
---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+$host = '127.0.0.1';
+$port = 3306;
+$dbname = 'php_dev';
+$user = 'Austin';
+$pass = 'bingbong';
+$charset = 'utf8mb4';
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try {
+    $db = new mysqli($host, $user, $pass, $dbname, $port);
+    $db->set_charset($charset);
+    $db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+} catch (\mysqli_sql_exception $e) {
+     throw new \mysqli_sql_exception($e->getMessage(), $e->getCode());
+}
+unset($host, $dbname, $user, $pass, $charset, $port); // we don't need them anymore 
+?>
