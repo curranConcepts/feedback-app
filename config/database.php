d4955cb8914c1e7d449ed52197c97a3613818c02
Austin_Curran <a.curran1192@gmail.com>
2023-10-13 14:27:50 -0600

    abstracted connection info into env file

--- config/database.php
+++ config/database.php
@@ -1,18 +1,14 @@
-<?php
-$host = '127.0.0.1';
-$port = 3306;
-$dbname = 'php_dev';
-$user = 'Austin';
-$pass = 'bingbong';
-$charset = 'utf8mb4';
+<?php include 'config/env.php'; ?>
 
+<?php
+//Variables for connection are set in config/env.php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
     $db = new mysqli($host, $user, $pass, $dbname, $port);
     $db->set_charset($charset);
     $db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
 } catch (\mysqli_sql_exception $e) {
-     throw new \mysqli_sql_exception($e->getMessage(), $e->getCode());
+    throw new \mysqli_sql_exception($e->getMessage(), $e->getCode());
 }
 unset($host, $dbname, $user, $pass, $charset, $port); // we don't need them anymore 
 ?>
